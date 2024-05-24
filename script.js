28d53390909db4d1d2674dc80c6ac8d5ab68b28a
FukcThat <63652e636f6465@gmail.com>
2024-05-24 22:46:19 +0200

    FEAT: Added Board in console

--- script.js
+++ script.js
@@ -1,1 +1,19 @@
-console.log(5);
+const gameBoard = (() => {
+  let board = [
+    ["o", " ", " "],
+    [" ", "x", " "],
+    [" ", " ", "o"],
+  ];
+
+  const getCurrentBoard = () => board;
+
+  const printBoard = () => {
+    board.forEach((row) => {
+      console.log(row.join(" | "));
+    });
+  };
+
+  return { getCurrentBoard, printBoard };
+})();
+
+gameBoard.printBoard();
